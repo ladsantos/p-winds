1f534ee7b01c0a5e35f89f7c014b0129996ada7c
leonardo <leonardo.dossantos@unige.ch>
2021-02-04 13:26:59 +0100

    Make it an installable package

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # p-winds
- Python implementation of Parker wind models for exoplanet atmospheres
+ Python implementation of Parker wind models for exoplanet atmospheres.
+
+Aims
+----
+The main objective of this code is to produce a simplified, 1-D mode of the upper atmosphere of a planet.
+
+Background
+----------
+`p-winds` is largely based on the theoretical framework of [Oklopčić & Hirata (2018)](https://ui.adsabs.harvard.edu/abs/2018ApJ...855L..11O/abstract) and [Lampón et al. (2020)](https://ui.adsabs.harvard.edu/abs/2020A%26A...636A..13L/abstract), which themselves based their work on the stellar wind model of [Parker (1958)](https://ui.adsabs.harvard.edu/abs/1958ApJ...128..664P/abstract).
---
+++ setup.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist upload")
+    sys.exit()
+
+setup(
+    name="p_winds",
+    version="0.1a",
+    author="Leonardo dos Santos",
+    author_email="Leonardo.dosSantos@unige.ch",
+    packages=["p_winds"],
+    url="https://github.com/ladsantos/p-winds",
+    license="MIT",
+    description="Parker wind models for planetary atmospheres",
+    install_requires=[line.strip() for line in
+                      open('requirements.txt', 'r').readlines()],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ]
+)
