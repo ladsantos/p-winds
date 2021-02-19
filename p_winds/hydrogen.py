Updated hydrogen.py to allow for the input of an average ion fraction to calculate correct sound speeds

p_winds/hydrogen.py
@@ -86,7 +86,7 @@ def recombination_rate(temperature):
 # Fraction of ionized hydrogen vs. radius profile
 def ion_fraction(radius_profile, planet_radius, temperature, h_he_fraction,
                  mass_loss_rate, planet_mass, spectrum_at_planet,
-                 initial_state=np.array([1.0, 0.0])):
+                 average_ion_fraction=0.0, initial_state=np.array([1.0, 0.0])):
     """
     Calculate the fraction of ionized hydrogen in the upper atmosphere in
     function of the radius in unit of planetary radius.
@@ -117,6 +117,9 @@ def ion_fraction(radius_profile, planet_radius, temperature, h_he_fraction,
         hydrogen (13.6 eV, or 911.65 Angstrom). Can be generated using
         ``tools.make_spectrum_dict``.
 
+    average_ion_fraction (``float``):
+        Average ion fraction in the upper atmosphere.
+
     initial_state (``numpy.ndarray``, optional):
         The initial state is the `y0` of the differential equation to be solved.
         This array has two items: the initial value of `f_ion` (ionization
@@ -136,7 +139,8 @@ def ion_fraction(radius_profile, planet_radius, temperature, h_he_fraction,
     # First calculate the sound speed, radius at the sonic point and the
     # density at the sonic point. They will be useful to change the units of
     # the calculation aiming to avoid numerical overflows
-    vs = parker.sound_speed(temperature, h_he_fraction).to(u.km / u.s)
+    vs = parker.sound_speed(temperature, h_he_fraction, average_ion_fraction
+                            ).to(u.km / u.s)
     rs = parker.radius_sonic_point(planet_mass, vs).to(u.jupiterRad)
     rhos = parker.density_sonic_point(mass_loss_rate, rs, vs).to(
         u.g / u.cm ** 3)

tests/test_hydrogen.py
@@ -8,7 +8,7 @@ from p_winds import hydrogen, tools
 
 # Let's test if the code is producing reasonable outputs. The ``ion_fraction()``
 # function for HD 209458 b should produce a profile with an ion fraction of
-# approximately one near the planetary surface, and approximately zero in the
+# approximately one near the planetary surface, and approximately 4E-4 in the
 # outer layers.
 def test_ion_fraction(precision_threshold=1E-5):
     units = {'wavelength': u.angstrom, 'flux': u.erg / u.s / u.cm ** 2 /
@@ -22,9 +22,10 @@ def test_ion_fraction(precision_threshold=1E-5):
     m_dot = 8E10 * u.g / u.s
     T_0 = 9E3 * u.K
     h_he = 0.9
+    average_f_ion = 0.7
 
     r = np.linspace(1, 15, 500)
     f_r, tau_r = hydrogen.ion_fraction(r, R_pl, T_0, h_he, m_dot, M_pl,
-                                       spectrum)
+                                       spectrum, average_f_ion)
     assert abs(f_r[-1] - 1.0) < precision_threshold
-    assert abs(f_r[0]) < precision_threshold
+    assert abs(f_r[0] - 4E-4) < precision_threshold