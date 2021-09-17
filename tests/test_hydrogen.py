3e346dfa1f0f44e857512446ce05975df46078db
leonardo <leonardo.dossantos@unige.ch>
2021-09-17 08:48:50 +0200

    Updated tests for `hydrogen.py`

--- tests/test_hydrogen.py
+++ tests/test_hydrogen.py
@@ -32,11 +32,11 @@ def test_ion_fraction_spectrum(precision_threshold=1E-5):
                                 average_f_ion,
                                 spectrum_at_planet=spectrum,
                                 relax_solution=True)
-    assert abs((f_r[-1] - 0.998199) / f_r[-1]) < precision_threshold
+    assert abs((f_r[-1] - 0.99808) / f_r[-1]) < precision_threshold
 
     # Test the exact photoionization
     f_r = hydrogen.ion_fraction(r, R_pl, T_0, h_fraction, m_dot, M_pl,
                                 average_f_ion,
                                 spectrum_at_planet=spectrum,
                                 relax_solution=True, exact_phi=True)
-    assert abs((f_r[-1] - .99799) / f_r[-1]) < precision_threshold
+    assert abs((f_r[-1] - .997896) / f_r[-1]) < precision_threshold
