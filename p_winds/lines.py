89d23b96d19a1fe38238ddec321a54cbc1bd4ce5
jseideleso <105068105+jseideleso@users.noreply.github.com>
2024-07-22 04:04:16 -0400

    Balmer line functionality
    Added the necessary function in line.py and hydrogen.py to enable Balmer line modelling.

--- p_winds/lines.py
+++ p_winds/lines.py
@@ -6,7 +6,7 @@ escape detection.
 """
 
 
-__all__ = ["he_3_properties", "c_ii_properties", "o_i_properties"]
+__all__ = ["he_3_properties", "c_ii_properties", "o_i_properties", "balmer_halpha_properties", "balmer_hbeta_properties", "balmer_hgamma_properties"]
 
 
 # Line properties of the 1.083 microns He triplet taken from the NIST database
@@ -161,3 +161,134 @@ def o_i_properties():
     a_ij_2 = 6.76e+07
 
     return lambda_0, lambda_1, lambda_2, f_0, f_1, f_2, a_ij_0, a_ij_1, a_ij_2
+
+
+# Line properties of the Balmer series lines (alpha, beta, gamma) taken from the NIST
+# database https://www.nist.gov/pml/atomic-spectra-database
+def balmer_halpha_properties():
+    """
+    Returns the central wavelengths in air, shell number and the
+    Einstein coefficient of the Balmer lines. The
+    values were taken from the NIST database:
+    https://www.nist.gov/pml/atomic-spectra-database
+
+    Returns
+    -------
+    lambda_alpha : ``float``
+        Central wavelength in air of the H-alpha line in unit of m.
+
+    n_alpha : ``integer``
+        Shell number creating the H-alpha line (unitless).
+        
+    f_alpha : ``float``
+        Oscillator strength (unitless).
+    
+    g_alpha : ``float``
+        log10(g2f2n_alpha) taking into account the oscillator strength (f2n)
+        and the statistical weight (g2) of the H-alpha line (unitless).
+
+    a_alpha : ``float``
+        Einstein coefficient of the H-alpha line in unit of 1 / s.
+
+    """
+    # Central wavelengths in units of m
+    lambda_alpha = 6562.79 * 1E-10
+    
+    #shell number
+    n_alpha = 3
+    
+    #oscillator strength
+    f_alpha = 6.4108e-01
+    
+    #statistical weight
+    g_alpha = 10**(0.71)
+    
+    # Einstein coefficient in units of s ** (-1)
+    a_alpha = 4.4101e+07
+
+
+    return lambda_alpha, n_alpha, f_alpha, g_alpha, a_alpha
+
+def balmer_hbeta_properties():
+    """
+    Returns the central wavelengths in air, shell number and the
+    Einstein coefficient of the Balmer lines. The
+    values were taken from the NIST database:
+    https://www.nist.gov/pml/atomic-spectra-database
+
+    Returns
+    -------
+    lambda_beta : ``float``
+        Central wavelength in air of the H-beta line in unit of m.
+
+    n_beta : ``integer``
+        Shell number creating the H-beta line (unitless).
+    
+    f_beta : ``float``
+        Oscillator strength (unitless).
+    
+    g_beta : ``float``
+        log10(g2f2n_alpha) taking into account the oscillator strength (f2n)
+        and the statistical weight (g2) of the H-beta line (unitless).
+        
+    a_beta : ``float``
+        Einstein coefficient of the H-beta line in unit of 1 / s.
+    """
+    # Central wavelengths in units of m
+    lambda_beta = 4861.35 * 1E-10
+
+    # Oscillator strength
+    f_beta = 1.1938e-01
+    
+    #shell number
+    n_beta = 4
+    
+    #statistical weight
+    g_beta= 10**(-0.02)
+    
+    # Einstein coefficient in units of s ** (-1)
+    a_beta = 8.4193e+06
+
+    return lambda_beta, n_beta, f_beta, g_beta,  a_beta
+
+def balmer_hgamma_properties():
+    """
+    Returns the central wavelengths in air, shell number and the
+    Einstein coefficient of the Balmer lines. The
+    values were taken from the NIST database:
+    https://www.nist.gov/pml/atomic-spectra-database
+
+    Returns
+    -------
+    lambda_gamma : ``float``
+        Central wavelength in air of the H-gamma line in unit of m.
+
+    n_gamma : ``integer``
+        Shell number creating the H-gamma line (unitless).
+        
+    f_gamma : ``float``
+        Oscillator strength (unitless).
+        
+    g_gamma : ``float``
+        log10(g2f2n_alpha) taking into account the oscillator strength (f2n)
+        and the statistical weight (g2) of the H-gamma line (unitless).
+
+    a_gamma : ``float``
+        Einstein coefficient of the H-gamma line in unit of 1 / s.
+    """
+    # Central wavelengths in units of m
+    lambda_gamma = 4340.47 * 1E-10
+    
+    #shell number
+    n_gamma = 5
+    
+    # Oscillator strength
+    f_gamma = 4.4694e-02
+    
+    #statistical weight
+    g_gamma = 10**(-0.447)
+    
+    # Einstein coefficient in units of s ** (-1)
+    a_gamma = 2.5304e+06
+
+    return lambda_gamma, n_gamma, f_gamma, g_gamma, a_gamma
